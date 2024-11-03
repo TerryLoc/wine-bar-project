089fe3aff5f73991deb243ab10588ec3e04b7964
Terence Loughran <terryloughran@gmail.com>
2024-11-03 11:28:32 +0000

    style: design the home page & cellar page

--- win_bar/urls.py
+++ win_bar/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_views
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -13,3 +14,6 @@ urlpatterns = [
         "logout/", auth_views.LogoutView.as_view(next_page="winery"), name="logout"
     ),  # Logout view
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
