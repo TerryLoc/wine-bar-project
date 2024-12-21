2ab2b2564fbab1fe76d2f24e1dba637eb5d563bd
Terence Loughran <terryloughran@gmail.com>
2024-12-21 18:45:28 +0000

    Chore: Comments to urls.py

--- bookings/urls.py
+++ bookings/urls.py
@@ -2,12 +2,15 @@ from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView
 
+# Define the app name to refer to the app in the project
 app_name = "bookings"
 
+# Define the URL patterns for the app
 urlpatterns = [
     path("wine_cellar/", views.wine_list, name="wine_cellar"),
     path("book/<int:wine_id>/", views.book_wine, name="book"),
     path("register/", views.register, name="register"),
+    # Use the custom login view to handle user authentication
     path(
         "login/",
         views.CustomLoginView.as_view(template_name="bookings/login.html"),
