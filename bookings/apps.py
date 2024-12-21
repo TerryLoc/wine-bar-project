dc76e6be85cafff7a7a9d6a9acf82122ebfd71e2
Terence Loughran <terryloughran@gmail.com>
2024-12-21 18:50:02 +0000

    Chore: Add comment to app configuration

--- bookings/apps.py
+++ bookings/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
 
+# Define the configuration for the bookings app
 class BookingsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bookings'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "bookings"
