54200bb7586abb93f7ea7a919b15934139a54b4f
TerryLoc <terryloughran@gmail.com>
2025-04-05 15:42:59 +0100

    Fix: Update CSRF for production, remove unused security

--- win_bar/settings.py
+++ win_bar/settings.py
@@ -22,7 +22,7 @@ DEBUG = (
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".herokuapp.com"]
 
 # Used to determine whether to use the secure version of cookies
-CSRF_TRUSTED_ORIGINS = ["https://*.herokuapp.com"]
+CSRF_TRUSTED_ORIGINS = ["https://the-wine-bar-5c52e3b945e9.herokuapp.com"]
 
 # Application definition
 INSTALLED_APPS = [
@@ -135,17 +135,3 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGOUT_REDIRECT_URL = "winery"  # This will redirect to the home page after logging out
 LOGIN_REDIRECT_URL = "bookings:profile"  # direct to profile page
-
-if not DEBUG:
-    # Security settings for production
-    SECURE_SSL_REDIRECT = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
-    SECURE_BROWSER_XSS_FILTER = True
-    SECURE_CONTENT_TYPE_NOSNIFF = True
-    SECURE_HSTS_SECONDS = 31536000  # 1 year
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_PRELOAD = True
-    X_FRAME_OPTIONS = "DENY"
-    CSRF_COOKIE_HTTPONLY = True
-    SECURE_REFERRER_POLICY = "same-origin"
